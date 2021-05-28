5e1627ff76cd04dcdbb0fb63fd578eaac31ad33a
camilogiraldo27 <camilogc71@gmail.com>
2021-05-28 11:40:10 -0500

    cambio en codigo escalera_1

--- app/src/main/java/com/example/prueba/Escalera.kt
+++ app/src/main/java/com/example/prueba/Escalera.kt
@@ -3,6 +3,7 @@ package com.example.prueba
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
@@ -20,7 +21,6 @@ import java.util.*
 class Escalera : AppCompatActivity() {
 
     lateinit var hynu: ImageView
-    lateinit var pepe: ImageView
     lateinit var dank: ImageView
     lateinit var animation: Animation
     lateinit var animationm: Animation
@@ -31,11 +31,11 @@ class Escalera : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_escalera)
 
-        val casillas = arrayListOf<ImageView>(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21,s22,s23,s24,s25,s26,s27,s28,s29,s30)
-        val imgcasillas = mutableListOf<Int>(casilla1, casilla2, casilla3, casiilla4, casilla5, casilla6, casilla7, casilla8, casilla9, casilla10, casilla11, casilla12, casilla13, casilla14, agujero1, casilla16, casilla17, casilla18, casilla19, nave, casilla21, casilla22, casilla23, casilla24, casilla25, agujero2, casilla27, casilla28, casilla29, casilla30)
+        val casillas = listOf<ImageView>(s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21,s22,s23,s24,s25,s26,s27,s28,s29,s30)
+        val imgcasillas = mutableListOf(casilla1, casilla2, casilla3, casiilla4, casilla5, casilla6, casilla7, casilla8, casilla9, casilla10, casilla11, casilla12, casilla13, casilla14, agujero1, casilla16, casilla17, casilla18, casilla19, nave, casilla21, casilla22, casilla23, casilla24, casilla25, agujero2, casilla27, casilla28, casilla29, casilla30)
         hynu = findViewById(id.hynu)
         dank = findViewById(id.dank)
-        val diceImages = mutableListOf<Int>(d1, d2, d3, d4, d5, d6)
+        val diceImages = mutableListOf(d1, d2, d3, d4, d5, d6)
         val lanzamiento1 = mutableListOf<Int>(d1, d2, d3, d4, d5, d6)
         val imgq = mutableListOf<Int>(m1, m2, m3, m4, m5, m6)
 
@@ -47,17 +47,14 @@ class Escalera : AppCompatActivity() {
         animationm = AnimationUtils.loadAnimation(this@Escalera, anim.pepe)
 
         //posicion en el tablero
-        var posj1 = 0
-        var posj2 = 0
+        var posj1 = 1
+        var posj2 = 1
 
         //turno de cada jugador
         var turno = 0
         //jugador
-        var random = Random().nextInt(6) + 1
-        //maquina
-        var randonm = Random().nextInt(6) + 1
-        //lanzamiento1
-        var random1 = Random().nextInt(6) + 1
+        var random = dado()
+        var random1 = dado()
         var turnom = 1
 
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -116,12 +113,8 @@ class Escalera : AppCompatActivity() {
         }
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-         posj2 += randonm
         fun mover(view: View?) {
             if (dado_player.isPressed) {
-                posj2 += randonm
-                var random = Random().nextInt(6) + 1
                 if (turno == 0) {
                     //nueva posicion
                     posj1 += random
@@ -130,25 +123,26 @@ class Escalera : AppCompatActivity() {
                         //exacto una variable alien
                         hynu = casillas[posj1 - 1]
                         hynu.setImageResource(drawable.hynu)
-                        dank.setImageResource(imgcasillas[posj2 - 1])
                     }
                     casilla_point()
                 }
             }
-        }
-            fun mover2(view: View?){
+            var randonm = Random().nextInt(5) + 1
             if (dado_maquina.isPressed) {
-                posj1 += random
-                var randonm = Random().nextInt(6) + 1
                 if (turnom == 1) {
-                    Toast.makeText(this, "Dank avanza "+randonm+" casillas", Toast.LENGTH_SHORT).show()
+                    //nueva posicion
                     posj2 += randonm
+                    Toast.makeText(this, "Dank avanza "+randonm+" casillas", Toast.LENGTH_SHORT).show()
                     for (i in casillas) {
                         //exacto una variable alien
                         dank = casillas[posj2 - 1]
                         dank.setImageResource(drawable.dank)
                         hynu.setImageResource(imgcasillas[posj1 - 1])
-
+                        if(casillas[posj2 - 1] == casillas[posj1 - 1]) {
+                            Toast.makeText(this, "Dank te alcanzo retrosede 3 casilla", Toast.LENGTH_SHORT).show()
+                            hynu = casillas[posj1 - 1]
+                            hynu.setImageResource(drawable.hynu)
+                        }
                     }
                 }
             }
@@ -158,7 +152,7 @@ class Escalera : AppCompatActivity() {
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         fun get_img_primerlanzamiento() {
-            random1 = Random().nextInt(6) + 1
+            random1 = Random().nextInt(5) + 1
             // start the animation
             primer_lanzamiento.startAnimation(animation)
 
@@ -185,8 +179,8 @@ class Escalera : AppCompatActivity() {
         // -----------------------------------------------------------------------------------------------------------------------------------------------------------------
         // -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-        fun get_img_dadoplayer() {
-            var random = Random().nextInt(6) + 1
+        fun get_img_dadoplayer(functionfinal:()->Unit) {
+            random=dado()
             // start the animation
             dado_player.startAnimation(animation)
 
@@ -199,6 +193,7 @@ class Escalera : AppCompatActivity() {
                 override fun onAnimationEnd(p0: Animation?) {
                     // when animation ends we will set the dice image
                     dado_player.setImageResource(diceImages.elementAt(random)) // this will set the dice image randomly
+                    functionfinal()
                     // enable the button click
                 }
 
@@ -213,7 +208,7 @@ class Escalera : AppCompatActivity() {
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
         fun get_img_dadomaquina() {
-            var randonm = Random().nextInt(6) + 1
+            var randonm = Random().nextInt(5) + 1
             // start the animation
             dado_maquina.startAnimation(animationm)
 
@@ -248,14 +243,12 @@ class Escalera : AppCompatActivity() {
         //volver a principal
         val btn_dadoplayer = findViewById<ImageView>(id.dado_player)
         btn_dadoplayer.setOnClickListener {
-            get_img_dadoplayer()
-            mover(hynu)
-            QuitarDadoPlayer()
-            get_img_dadomaquina()
-            mover2(dank)
-            QuitarDadoMaquina()
-        }
+            get_img_dadoplayer {
+                mover(hynu)
+                QuitarDadoPlayer()
+            }
 
+        }
         val ms = findViewById<ImageView>(R.id.musica)
         ms.setOnClickListener {
             val es = Intent(this, Musica::class.java)
@@ -266,10 +259,16 @@ class Escalera : AppCompatActivity() {
             val ajust = Intent(this, Ajustes::class.java)
             startActivity(ajust)
         }
-
+        //volver a principal
+        val btn_dadomaquina = findViewById<ImageView>(id.dado_maquina)
+        btn_dadomaquina.setOnClickListener {
+            get_img_dadomaquina()
+            mover(dank)
+            QuitarDadoMaquina()
+        }
         val btn_firstlanzamiento = findViewById<ImageView>(id.primer_lanzamiento)
         btn_firstlanzamiento.setOnClickListener {
-            random1 = Random().nextInt(6) + 1
+            random1 = Random().nextInt(5) + 1
             get_img_primerlanzamiento()
             when (random1) {
                 5 -> {
@@ -282,6 +281,14 @@ class Escalera : AppCompatActivity() {
                 }
             }
         }
+
+
+    }
+    fun dado():Int{
+
+        var random =Random().nextInt(5) + 1
+        Log.d("MiLog", random.toString())
+        return random
     }
 }
 
